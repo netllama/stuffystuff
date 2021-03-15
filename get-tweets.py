cb2092f921d70da4a74969b34e80df0060a72646
Lonni J Friedman <netllama@gmail.com>
2021-03-15 16:23:22 -0700

    * updating to python3

--- get-tweets.py
+++ get-tweets.py
@@ -1,4 +1,4 @@
-#!/bin/python2.7
+#!/bin/python3
 """Get twitter timeline and convert to XML formatted RSS feed."""
 
 import psycopg2
@@ -9,12 +9,9 @@ import pytz
 import re
 from curses import ascii
 from feedgen.feed import FeedGenerator
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
 
 
-CONN_STRING = ''
+CONN_STRING = 'host=127.0.0.1 dbname="" user="" password="" port=5432'
 
 def main():
     """Main func."""
@@ -53,7 +50,7 @@ def db_qry(sql_pl, operation):
         conn = psycopg2.connect(CONN_STRING)
         conn.autocommit = True
     except Exception as err:
-        print 'Database connection failed due to error:\t{}'.format(err)
+        print('Database connection failed due to error:\t{}'.format(err))
         return False
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     # invoke the SQL
@@ -62,7 +59,7 @@ def db_qry(sql_pl, operation):
         row_count = cursor.rowcount
     except Exception as err:
         # query failed
-        print 'SQL ( {} )\t failed due to error:\t{}'.format(cursor.query, err)
+        print('SQL ( {} )\t failed due to error:\t{}'.format(cursor.query, err))
         ret_val = False
     if operation == 'select' and ret_val:
         # get returned data to return
@@ -86,8 +83,8 @@ def max_id():
     first_last_sql = 'SELECT min(id), max(id) FROM tweets'
     first_last_qry = db_qry([first_last_sql, None], 'select')
     if not first_last_qry and not first_last_qry[0]:
-    	print 'FAILED to get first or last id:\t{}'.format(first_last_qry)
-	return last_id, first_id
+        print('FAILED to get first or last id:\t{}'.format(first_last_qry))
+        return last_id, first_id
     last_id = first_last_qry[0][-1]
     first_id = first_last_qry[0][0]
     return last_id, first_id
@@ -117,13 +114,13 @@ def get_tweets(last_id):
     	for item in tweepy.Cursor(t_api.home_timeline, since_id=last_id, tweet_mode='extended').items():
     	    latest_timeline.append(item)
     except tweepy.TweepError as err:
-    	print 'FAILED to get home_timeline due to error:\t{}'.format(err)
+    	print('FAILED to get home_timeline due to error:\t{}'.format(err))
     try:
     	for item in tweepy.Cursor(t_api.mentions_timeline, since_id=last_id, tweet_mode='extended').items():
 	    #print '{}\t{}'.format(item.author.screen_name, item.text)
     	    latest_timeline.append(item)
     except tweepy.TweepError as err:
-    	print 'FAILED to get mentions_timeline due to error:\t{}'.format(err)
+    	print('FAILED to get mentions_timeline due to error:\t{}'.format(err))
     if latest_timeline:
     	# remove duplicate keys
     	tweets = {t.id_str: t for t in latest_timeline}
@@ -144,33 +141,33 @@ def insert_tweets(tweets):
     tweets_sql_vals = []
     tweets_payload = []
     for tweet_data in tweets:
-    	# convert UTC to localtime
-	local_tz = utc_to_local(tweet_data.created_at)
-	#print tweet_data.entities['urls']
-	if not tweet_data.entities['urls']:
-	    # tweet has no explicit URL!?
+        # convert UTC to localtime
+        local_tz = utc_to_local(tweet_data.created_at)
+        #print tweet_data.entities['urls']
+        if not tweet_data.entities['urls']:
+            # tweet has no explicit URL!?
             # perform regex match for URL
-	    # print 'NO URL:\t{}'.format(tweet_data.text)
+            # print 'NO URL:\t{}'.format(tweet_data.text)
             urls = re.findall(url_regex, tweet_data.full_text)
             if urls:
                 status_url = urls[-1:]
             else:
-	        status_url = ['https://twitter.com']
-	else:
-	    status_url = [tweet_data.entities['urls'][0]['expanded_url']]
-	author = tweet_data.author.screen_name
-    	tweet_vals = [tweet_data.id_str, local_tz.replace(tzinfo=None),
-	    	      author, tweet_data.full_text]
-	tweets_sql_vals.append(tweet_vals)
-	tweets_payload.append(tweet_vals + status_url)
+                status_url = ['https://twitter.com']
+        else:
+            status_url = [tweet_data.entities['urls'][0]['expanded_url']]
+        author = tweet_data.author.screen_name
+        tweet_vals = [tweet_data.id_str, local_tz.replace(tzinfo=None),
+                      author, tweet_data.full_text]
+        tweets_sql_vals.append(tweet_vals)
+        tweets_payload.append(tweet_vals + status_url)
     insert_sql = 'INSERT INTO tweets (id, tstamp, username, tweet) VALUES (%s, %s, %s, %s)'
     insert_worked = True
     for row in tweets_sql_vals:
     	#print row
     	insert_status = db_qry([insert_sql, row], 'insert')
     	if not insert_status:
-	    insert_worked = False
-	    print 'Failed to insert:\t{}\t{}'.format(insert_sql, row)
+            insert_worked = False
+            print('FAILED to insert:\t{}\t{}'.format(insert_sql, row))
     return insert_worked, tweets_payload
 
 
@@ -179,9 +176,9 @@ def parse_url(desc):
     new_desc = desc
     desc_list = desc.split(' ')
     if desc_list and desc_list[-1].startswith('http'):
-    	url = desc_list[-1]
-	html_url = '<A HREF="{u}">{u}</a>'.format(u=url)
-	new_desc = desc.replace(url, html_url)
+        url = desc_list[-1]
+        html_url = '<A HREF="{u}">{u}</a>'.format(u=url)
+        new_desc = desc.replace(url, html_url)
     return u'{}'.format(new_desc)
 
 
@@ -198,23 +195,23 @@ def make_rss_feed(tweets):
     fg.description('twitter for netllama')
     fg.updated(build_date)
     for tweet in tweets:
-    	fe = fg.add_item()
-    	fe.guid(tweet[0])
-    	# need to re-add TZ info to make this work
-    	fe.pubDate(tweet[1].replace(tzinfo=tz))
-	#desc = clean(tweet[3])
-	#desc = u'{}'.format(tweet[3].encode('utf-8'))
-	desc = parse_url(tweet[3].encode('utf-8'))
-	# author: desc
-	title = u'{}: {}'.format(tweet[2], desc)
-    	fe.title(title)
-    	fe.description(desc)
-    	fe.link(href=tweet[4])
+        fe = fg.add_item()
+        fe.guid(tweet[0])
+        # need to re-add TZ info to make this work
+        fe.pubDate(tweet[1].replace(tzinfo=tz))
+        #desc = clean(tweet[3])
+        #desc = u'{}'.format(tweet[3].encode('utf-8'))
+        desc = parse_url(tweet[3])
+        # author: desc
+        title = u'{}: {}'.format(tweet[2], desc)
+        fe.title(title)
+        fe.description(desc)
+        fe.link(href=tweet[4])
     try:
     	rssfeed = fg.rss_str(pretty=True)
     	fg.rss_file(fpath)
     except Exception as err:
-    	print 'Failed to generate RSS feed due to error:\t{}'.format(err)
+    	print('FAILED to generate RSS feed due to error:\t{}'.format(err))
     	feed_built = False
     return feed_built
 
@@ -224,7 +221,7 @@ def delete_old_tweets(last_id):
     delete_sql = 'DELETE FROM tweets WHERE id < %s AND id NOT IN (SELECT id FROM tweets ORDER BY id DESC LIMIT 15)'
     delete_status = db_qry([delete_sql, [last_id]], 'delete')
     if not delete_status:
-    	print 'Failed to delete last_id ({}):\t{}'.format(last_id, delete_sql)
+    	print('FAILED to delete last_id ({}):\t{}'.format(last_id, delete_sql))
 
 
 if __name__ == "__main__":
